3795701a60c45a6f02f7c7ed9855fe4c4142c574
Om Kumar <edahnXC@gmail.com>
2026-02-22 22:09:49 +0530

    Database migration completed and .vs folder ignored

--- PortfolioAPI/Models/Admin.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PortfolioAPI.Models
-{
-    public class Admin
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
---
+++ backend/PortfolioAPI/Models/Poem.cs
@@ -0,0 +1,12 @@
+﻿namespace PortfolioAPI.Models
+{
+    public class Poem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate{  get; set; }
+
+        public string Category { get; set;  }
+    }
+}
