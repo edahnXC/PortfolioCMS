21c6adc01f24e3745f5217c97c3fd86d8211af14
Om Kumar <edahnXC@gmail.com>
2026-02-22 14:59:36 +0530

    Initial full-stack project setup with Angular frontend and ASP.NET backend

---
+++ PortfolioAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortfolioAPI.Models;
+
+namespace PortfolioAPI.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Poem> Poems { get; set; }
+
+        public DbSet<Photo> Photos { get; set; }
+    }
+}
+
