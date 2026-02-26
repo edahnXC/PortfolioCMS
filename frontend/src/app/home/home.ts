d939d0b66ecb8a7ba292063c4e16ea31a8ed46bd
Om Kumar <edahnXC@gmail.com>
2026-02-27 01:51:04 +0530

    poems tab working now and all poems can be seen

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,31 +1,40 @@
-import { Component, OnInit} from '@angular/core';
-import {CommonModule} from "@angular/common";
-import {PoemService} from "../services/poem";
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { PoemService } from "../services/poem";
 import { PhotoService } from '../services/photo';
+import { ChangeDetectorRef } from '@angular/core';
+
 @Component({
   selector: 'app-home',
-  standalone:true,
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
 })
 export class Home implements OnInit {
-poems:any[]=[];
-photos:any[]=[];
 
-constructor(private poemService:PoemService,
-  private photoService:PhotoService
-){}
+  poems: any[] = [];
+  photos: any[] = [];
 
-ngOnInit() {
-  this.poemService.getAllPoems().subscribe(
-    (data) => {
-      this.poems = data.slice(0,2);// show only latest 2 poems
-    });
+  constructor(
+    private poemService: PoemService,
+    private photoService: PhotoService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
-  this.photoService.getAllPhotos()
+  ngOnInit() {
+
+    this.poemService.getAllPoems()
+      .subscribe(data => {
+        this.poems = data.slice(0,2);
+        this.cdr.detectChanges();
+      });
+
+    this.photoService.getAllPhotos()
       .subscribe(data => {
         this.photos = data.slice(0,3);
+        this.cdr.detectChanges();
       });
-}
+
+  }
 }
--- frontend/src/app/poems/poems.ts
+++ frontend/src/app/poems/poems.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PoemService } from '../services/poem';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-poems',
@@ -13,12 +14,17 @@ export class Poems implements OnInit {
 
   poems: any[] = [];
 
-  constructor(private poemService: PoemService) {}
+  constructor(
+    private poemService: PoemService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     this.poemService.getAllPoems()
       .subscribe(data => {
+        console.log("POEMS:", data);
         this.poems = data;
+        this.cdr.detectChanges();   // FORCE UI UPDATE
       });
   }
 }
